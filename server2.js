8a22a7a6646d0fa3ac5165d9648207e510f1b2c5
GUS <GUS>
2016-12-10 13:58:32 -0500

    removed files

--- server2.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var server = http.createServer(function(request,response){
-	console.log('got a request');
-	console.log(request.url);
-	response.write('hello');
-	response.end();
-})
-
-server.listen(8080);
